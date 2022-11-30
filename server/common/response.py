936ca10f87fa042bffee3da5df83bb8c489b5654
野漫横江 <1399226280@qq.com>
2022-11-30 17:18:48 +0800

    refactor: 修改response

--- server/common/response.py
+++ server/common/response.py
@@ -5,23 +5,27 @@ from flask import make_response, jsonify
 class Response():
     """ Http Response """
 
-    def __init__(self, data: Dict=None, message: Union[str, None] = None, code: int = 0):
-        self.response = {"data": data, "message": message, "code": code}
+    def __init__(self, data: Dict = None, message: Union[str, None] = None, code: int = 0):
+        self.__response = {"data": data, "message": message, "code": code}
 
-    def __update(self, data: Dict=None, message: Union[str, None] = None, code: int = 0):
-        self.response = {"data": data, "message": message, "code": code}
+    def __update(self, data: Dict = None, message: Union[str, None] = None, code: int = 0):
+        self.__response = {"data": data, "message": message, "code": code}
 
-    def ok(self, data: Dict=None, message: Union[str, None] = "操作成功"):
+    def ok(self, data: Dict = None, message: Union[str, None] = "操作成功"):
         """操作成功"""
         self.__update(data, message, 20000)
-        return make_response(jsonify(self.response))
+        return make_response(jsonify(self.__response))
 
-    def error(self, data: Dict=None, message: Union[str, None] = "操作失败"):
+    def error(self, data: Dict = None, message: Union[str, None] = "操作失败"):
         """操作失败"""
         self.__update(data, message, 20001)
-        return make_response(jsonify(self.response))
+        return make_response(jsonify(self.__response))
 
-    def relogin(self):
+    def to_dict(self) -> Dict:
+        return self.__response
+
+    @staticmethod
+    def relogin():
         """重新登录"""
-        self.__update(None, "登录失败, 请重新登录", 30001)
-        return make_response(jsonify(self.response))
+        response = Response(None, "登录失败, 请重新登录", 30001)
+        return make_response(jsonify(response.to_dict()))
