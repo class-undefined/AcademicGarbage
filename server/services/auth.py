112e743b5936066a7ae42ab6542cf486340b17ca
野漫横江 <1399226280@qq.com>
2022-11-30 14:58:21 +0800

    docs:

--- server/services/auth.py
+++ server/services/auth.py
@@ -8,14 +8,17 @@ serializer = URLSafeSerializer(GLOBAL_SECRET_KEY, salt=GLOBAL_SALT)
 
 
 def encode_data(data: Any) -> Union[str, bytes]:
+    """数据编码"""
     return serializer.dumps(data)
 
 
 def decode_data(s: Union[str, bytes]) -> Any:
+    """数据解码"""
     return serializer.loads(s)
 
 
-def test_decode():
+def test_encode_decode():
+    """单元测试-测试编码解码"""
     datas = ['123', 123, True, {"a": True}, ['good', 2]]
     secret = encode_data(datas)
     rst = decode_data(secret)
