8999cdb8f215ec407472fa12576717b7ff6a0dd0
野漫横江 <1399226280@qq.com>
2022-12-02 23:10:52 +0800

    fix: 忘记调用to_response

--- server/controller/user.py
+++ server/controller/user.py
@@ -42,12 +42,12 @@ def login(data: Union[User, None]):
         if is_login:
             response_data["user"] = user.to_vo()
             response_data["token"] = user.generate_token()
-            return Response().ok(data=response_data, message="登录成功")
+            return Response().ok(data=response_data, message="登录成功").to_response()
         raise RequestError("账户或密码错误")
     # token验证成功, 返回用户
     if data is not None:
         response_data["user"] = data.to_vo()
-        return Response().ok(data=response_data, message="登录成功")
+        return Response().ok(data=response_data, message="登录成功").to_response()
     # 否则重新登录
     return Response.relogin().to_response()
 
