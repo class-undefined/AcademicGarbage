2ce3754dc0f341a46803de77f052ec7d17e0f170
野漫横江 <1399226280@qq.com>
2022-12-02 23:17:43 +0800

    style: 应用层移除.to_response()

--- server/controller/user.py
+++ server/controller/user.py
@@ -17,7 +17,7 @@ def register():
     if "username" not in body or "password" not in body:
         return Response().error(message="缺少账户或密码!")
     user = User.register(**body)
-    return Response.ok(message="注册成功", data=user.to_vo()).to_response()
+    return Response.ok(message="注册成功", data=user.to_vo())
 
 
 @user_blue.route("/login", methods=["POST"])
@@ -31,7 +31,7 @@ def login(data: Union[User, None]):
     }
     # body为空且token验证失败, 需要重新登录
     if body is None and data is None:
-        return Response.relogin().to_response()
+        return Response.relogin()
     # 用登录参数优先验证登录参数
     if body is not None:
         if "username" not in body or "password" not in body:
@@ -42,14 +42,14 @@ def login(data: Union[User, None]):
         if is_login:
             response_data["user"] = user.to_vo()
             response_data["token"] = user.generate_token()
-            return Response().ok(data=response_data, message="登录成功").to_response()
+            return Response().ok(data=response_data, message="登录成功")
         raise RequestError("账户或密码错误")
     # token验证成功, 返回用户
     if data is not None:
         response_data["user"] = data.to_vo()
-        return Response().ok(data=response_data, message="登录成功").to_response()
+        return Response().ok(data=response_data, message="登录成功")
     # 否则重新登录
-    return Response.relogin().to_response()
+    return Response.relogin()
 
 
 @user_blue.route("/history", methods=["GET"])
@@ -57,9 +57,9 @@ def login(data: Union[User, None]):
 @interceptor()
 def history(data: Union[User, None]):
     if data is None:
-        return Response.relogin().to_response()
+        return Response.relogin()
     # 暂不考虑分页
     photos = []
     for photo in data.photos:
         photos.append(photo.to_dict())
-    return Response.ok(data=photos).to_response()
+    return Response.ok(data=photos)
