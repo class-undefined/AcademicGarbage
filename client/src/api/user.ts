db88060ab62819b1fb61dfc4daea78a51f942e68
野漫横江 <1399226280@qq.com>
2022-12-03 16:34:11 +0800

    feat: 登录接口

---
+++ client/src/api/user.ts
@@ -0,0 +1,15 @@
+import { service } from "@/utils/api/service/service"
+import { Response } from "@/utils/api/response/type"
+import { IUser } from "@/types/user"
+type LoginResult = {
+    token: string
+    user: IUser
+}
+
+export const login = (username: string, password: string) => {
+    return service({
+        url: "/user/login",
+        method: "POST",
+        data: { username, password },
+    }) as unknown as Promise<Response<LoginResult>>
+}
--- client/src/store/global.ts
+++ client/src/store/global.ts
@@ -1,5 +1,8 @@
+import { login } from "@/api/user"
 import { User } from "@/types/user"
+import { removeToken } from "@/utils/api/auth/auth"
 import { defineStore } from "pinia"
+import { useRouter } from "vue-router"
 export const useGlobalStore = defineStore("global", {
     state: () => {
         return {
@@ -7,6 +10,15 @@ export const useGlobalStore = defineStore("global", {
         }
     },
     actions: {
-        login(username: string, password: string) {},
+        login(username: string, password: string) {
+            login(username, password).then(({ data }) => {
+                this.user = new User(data.user)
+            })
+        },
+        logout() {
+            this.user = null
+            removeToken()
+            useRouter().replace("/login")
+        },
     },
 })
