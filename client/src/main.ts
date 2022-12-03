081f42d1c2c613581923ca7e5826af094cf3082a
野漫横江 <1399226280@qq.com>
2022-12-03 15:41:21 +0800

    feat: 引入router, 增加路径别名

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue"
+import { createPinia } from "pinia"
+import router from "./router"
+import App from "./App.vue"
+import "./style.css"
+import "vant/es/toast/style"
+import "vant/es/dialog/style"
+import "vant/es/notify/style"
+import "vant/es/image-preview/style"
 
-createApp(App).mount('#app')
+createApp(App).use(createPinia()).use(router).mount("#app")
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import Components from "unplugin-vue-components/vite"
+import { VantResolver } from "unplugin-vue-components/resolvers"
+import { resolve } from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+	plugins: [
+		vue(),
+		Components({
+			resolvers: [VantResolver()],
+		}),
+	],
+	resolve: {
+		alias: {
+			"@": resolve("src"),
+			"@assets": resolve("src/assets"),
+			"@comps": resolve("src/components"),
+			"@utils": resolve("src/utils"),
+			"@router": resolve("src/router"),
+			"@store": resolve("src/store"),
+		},
+	},
 })
