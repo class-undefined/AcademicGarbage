d058819926b4db67a9d271d4ddce5a7175b3f3e1
野漫横江 <1399226280@qq.com>
2022-12-03 10:37:44 +0800

    feat: oss上传后可得到url

--- server/common/oss/manage.py
+++ server/common/oss/manage.py
@@ -12,21 +12,28 @@ class OssManage():
         bucket_name = config["bucket"]
         auth = oss2.Auth(access_key_id, access_key_secret)
         self.bucket = oss2.Bucket(auth, endpoint, bucket_name)
+        self.prefix = f"https://{bucket_name}.{endpoint}"
 
     def __read_oss_config(self):
         config_path = "server/common/oss/config.yaml"
         stream = open(config_path, "rb")
         return yaml.load(stream, yaml.SafeLoader)
 
-    def get_object(self, key: str) -> Any:
+    def get_object(self, filename: str) -> Any:
         """获得一个oss对象"""
-        return self.bucket.get_object(key)
+        return self.bucket.get_object(filename)
 
-    def upload(self, key: str, data: Any, progress_callback=None):
+    def upload(self, filename: str, data: Any, progress_callback=None):
         """上传数据至oss"""
-        self.bucket.put_object(key, data, progress_callback)
+        rst = self.bucket.put_object(filename, data, progress_callback)
+        return self.get_url(filename)
+
+    def get_url(self, filename: str) -> str:
+        """获得文件下载地址"""
+        return f"{self.prefix}/{filename}"
 
 
 def test():
     oss = OssManage()
-    oss.upload("foo.txt", "bar")
+    rst = oss.upload("foo.txt", "bar")
+    assert rst == "https://bins-1.oss-cn-hangzhou.aliyuncs.com/foo.txt"
