0e133bf1e1fa7bb53c3cfdb55650de5fc09f3ca9
class-undefined <class-undefined@qq.com>
2022-12-16 01:59:28 +0800

    fix: 修复路由未正常跳转

--- client/src/views/Layout.vue
+++ client/src/views/Layout.vue
@@ -3,13 +3,16 @@ import { routes } from "@router/index"
 import { useRouter } from "vue-router"
 import { watch, ref, computed } from "vue"
 const router = useRouter()
-
+const handleClick = (path: string) => {
+    router.push(path)
+}
 const tabs = routes
     .filter(route => route?.meta?.navbar)
     .map(route => {
         return {
             title: route.meta?.title as string,
             icon: route.meta?.icon as undefined,
+            path: route.path
         }
     })
 
@@ -27,11 +30,13 @@ const tabHeight = computed(() => {
 
 <template>
     <van-config-provider theme="dark">
-        <div class="main" :style="{ height: `calc(100% - ${tabHeight}px)` }"><RouterView /></div>
+        <div class="main" :style="{ height: `calc(100% - ${tabHeight}px)` }">
+            <RouterView />
+        </div>
     </van-config-provider>
     <div>
         <van-tabbar v-if="current.meta?.navbar" v-model="current.path">
-            <van-tabbar-item v-for="tab in tabs" :key="tab.title" :icon="tab.icon">
+            <van-tabbar-item v-for="tab in tabs" :key="tab.title" @click="handleClick(tab.path)" :icon="tab.icon">
                 {{ tab.title }}
             </van-tabbar-item>
         </van-tabbar>
